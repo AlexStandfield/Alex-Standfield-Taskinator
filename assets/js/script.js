99edf3279428b3dc23667627cbc491eb472fc25e
Alex Standfield <AlexStandfield@gmail.com>
2022-05-28 13:13:35 -0600

    lesson 2 but code is not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,31 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function (event) {
+var taskFormHandler = function(event) {
   event.preventDefault();
-  var taskNameInput = document.querySelector("input[name='ask-name']").value;
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+  // Check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return fasle;
+  }
+
+  formEl.reset();
 
   // Package up data like an object
   var taskDataObj = {
     name: taskNameInput,
-    input: taskTypeInput
+    type: taskTypeInput,
   };
 
   // Send it as an arcument to createTaskEl
   createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskDataObj) {
-     // Create List Item
+var createTaskEl = function (taskDataObj) {
+  // Create List Item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
@@ -26,12 +34,18 @@ var createTaskEl = function(taskDataObj) {
   // Give it a class name
   taskInfoEl.className = "task-info";
   // Add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.input + "</span>";
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskDataObj.name +
+    "</h3><span class='task-type'>" +
+    taskDataObj.type +
+    "</span>";
 
   listItemEl.appendChild(taskInfoEl);
 
-// Add Entire list item to list
-  tasksToDoEl.appendChild(listItemEl);
-}
 
-formEl.addEventListener("click", taskFormHandler);
+  // Add Entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("sumbit", taskFormHandler);
